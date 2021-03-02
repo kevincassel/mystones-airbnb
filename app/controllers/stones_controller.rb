f835d1cfb61c87076bc8784258f4af9346e0e946
kevincassel <kc.lewagon@gmail.com>
2021-03-02 14:44:25 +0100

    skip login for show stones and create a view for show

--- app/controllers/stones_controller.rb
+++ app/controllers/stones_controller.rb
@@ -1,6 +1,6 @@
 class StonesController < ApplicationController
 
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @stones = Stone.all
