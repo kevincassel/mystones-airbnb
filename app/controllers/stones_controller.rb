02d98c74b0285da09a6f72ffd40282b5ecc2ec9a
simon lacote <simonlacote@hotmail.fr>
2021-03-04 14:49:33 +0100

    update city

--- app/controllers/stones_controller.rb
+++ app/controllers/stones_controller.rb
@@ -2,16 +2,6 @@ class StonesController < ApplicationController
 
   skip_before_action :authenticate_user!, only: [:index, :show]
 
-  # def index
-  #   @stones = Stone.all
-  #   @markers = @stones.geocoded.map do |stone|
-  #     {
-  #       lat: stone.latitude,
-  #       lng: stone.longitude,
-  #       infoWindow: render_to_string(partial: "info_window", locals: { stone: stone })
-  #     }
-  # end
-
   def index
     @stones = Stone.all
       @markers = @stones.geocoded.map do |stone|
@@ -24,15 +14,13 @@ class StonesController < ApplicationController
     if params[:query].present?
       sql_query = " \
       stones.name @@ :query \
-      OR stones.name @@ :query \
+      OR stones.town @@ :query \
       "
       @stones = Stone.joins(:user).where(sql_query, query: "%#{params[:query]}%")
     else
       @stones = Stone.all
     end
   end
-
-
 
   def show
     @stone = Stone.find(params[:id])
