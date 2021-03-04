fed0888966eb28e19a47051657088998fa0433a2
simon lacote <simonlacote@hotmail.fr>
2021-03-04 16:41:47 +0100

    add map to show and searchbar to home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,16 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if params[:query].present?
+      sql_query = " \
+      stones.name @@ :query \
+      OR stones.town @@ :query \
+      "
+      @stones = Stone.joins(:user).where(sql_query, query: "%#{params[:query]}%")
+    else
+      @stones = Stone.all
+    end
+
   end
+
 end
