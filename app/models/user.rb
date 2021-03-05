525042129d0c45c444d34f73c2e943caed0e9e39
simon lacote <simonlacote@hotmail.fr>
2021-03-05 11:42:53 +0100

    fixed booking pb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :stones, dependent: :destroy
   has_many :bookings, dependent: :destroy
-  has_many :stones, through: :bookings
+  has_many :stones
 end
